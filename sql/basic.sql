8f7e8cf36feecb13aae9da2cee9fee430b5abb4e
Hiu Kwok <hiufkwok@gmail.com>
2018-06-26 18:01:51 +0800

    SQL

---
+++ sql/basic.sql
@@ -0,0 +1,20 @@
+/*
+ * Purpose: 
+ *    - bababababba
+ *
+ * Noted: 
+ *
+ *  - Involved DB: test_DB (Oracle)
+ *
+ * Involed table:
+ *    - table_x 
+ *	  - table_y
+ *
+ * Content changed:
+ *  None
+ *
+ * Index:
+ *  1.1: Query used on Java class.
+ *  1.2: Dummy entry
+ *  1.3: Dummy clean up
+ */
